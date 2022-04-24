28ff8003c4a56128667535e96413ebb9d370ebfb
rahdnus <rahdnus119@gmail.com>
2022-04-24 16:09:21 +0530

    Updated Linear Check

--- src/MovementTranslator.cs
+++ src/MovementTranslator.cs
@@ -10,16 +10,20 @@ namespace Hanabi{
         [Space(20)]
         [SerializeField]MovementQuery query;
         Vector3 start,destination;
-        float T=-1,scaler=0.01f,step=1;
-        int operationCount;
+        float T=0,scaler=0.01f,step=1;
+        int operationCount,direction=1;
 
     public void Init()
     {
         step=query.speed*scaler;
         Operation=null;
+
+        operationCount=0;
+
         switch(query.type)
         {
             case MovementType.Linear:
+                direction=1;
                 InitLinear();
                 Operation+=Linear;
                 break;
@@ -32,43 +36,59 @@ namespace Hanabi{
     }
     public void Clear()
     {
-        T=-1;
         Operation=null;
         operationCount=0;
+        T=-1;
     }
     private void InitLinear()
     {
         start=transform.position;
-        
+
         float y=Mathf.Sin(Mathf.Deg2Rad*query.angle)*query.distance;
         float z=Mathf.Cos(Mathf.Deg2Rad*query.angle)*query.distance;
 
         destination=new Vector3(0,y,z)+start;
-        operationCount=0;
     }
     public void Linear()
     {  
-        CheckLinear();
-    
         transform.position=Vector3.Lerp(start,destination,T);
-        T+=step;
+        T+=step*direction;
+
+        CheckLinear();
+    }
+      void CheckLinear()
+    {
+        if ((T>=1 && direction==1) ||(T<=0 && direction==-1))
+        {
+            operationCount+=1;
+            direction*=-1;
+            
+            if(operationCount==query.times)
+            {
+                Arrived();
+                Clear();
+            }
+        }
     }
     private void InitRadial()
     {
         start=transform.position;
         T=query.angle;
+        direction=query.anticlockwise;
+
+        operationCount=0;
+
         float y=Mathf.Sin(Mathf.Deg2Rad*T)*query.distance;
         float z=Mathf.Cos(Mathf.Deg2Rad*T)*query.distance;
         
         destination=new Vector3(0,y,z)+start;
         Debug.Log(destination);
-        operationCount=0;
 
         Debug.Log(step);
     }
     public void Radial()
     {
-        T=(T+step)%360;
+        T=(T+step*query.anticlockwise)%360;
 
         float y=Mathf.Sin(Mathf.Deg2Rad*T)*query.distance;
         float z=Mathf.Cos(Mathf.Deg2Rad*T)*query.distance;
@@ -80,7 +100,7 @@ namespace Hanabi{
     void CheckRadial()
     {
 
-        if(T-query.angle<step)
+        if(Mathf.Abs(T-query.angle)<step)
         {
             T=query.angle;
             transform.position=destination;
@@ -95,20 +115,13 @@ namespace Hanabi{
            
         }
     }
-    void CheckLinear()
-    {
-        if (Vector3.Distance(transform.position, destination) <= float.Epsilon ||   operationCount==query.times)
-        {
-            Arrived();
-            Clear();
-        }
-    }
+  
 }
     [System.Serializable]
     public class MovementQuery{
         public MovementType type;
         public float angle=-1,distance=-1,speed=1;
-        public int times=1;
+        public int times=1,anticlockwise=1;
     }
     public enum MovementType
     {
